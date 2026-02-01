60b1c849b01ee557cfdb3eed59b71942083e7ab3
Nehal Mehta <nehal29096@gmail.com>
2026-02-01 23:37:10 +0530

    updated

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ require('./db');
 
 app.use(express.json());
 
+// Import task routes (CRUD in separate files)
+const createTask = require("./routes/createTask");
+const readTask = require("./routes/readTask");
+const updateTask = require("./routes/updateTask");
+const deleteTask = require("./routes/deleteTask");
+
+// Mount routes
+app.use("/api/tasks", createTask); // POST
+app.use("/api/tasks", readTask);   // GET, GET by ID
+app.use("/api/tasks", updateTask); // PUT
+app.use("/api/tasks", deleteTask); // DELETE
+
 //Test route
 app.get("/", (req,res) => {
     res.send("Ok");
